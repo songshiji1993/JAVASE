4c4cb3c3fc5d6d079cfc07984de96f027c2cbc35
song <383370142@qq.com>
2019-01-14 23:24:48 +0800

    test branch1

--- src/tcs/TestBranch2.java
+++
@@ -1,7 +0,0 @@
-package tcs;
-
-public class TestBranch2 {
-    public static void main(String[] args) {
-        System.out.println("测试分支2");
-    }
-}
---
+++ src/tcs/TestMasterBranch.java
@@ -0,0 +1,7 @@
+package tcs;
+
+public class TestMasterBranch {
+    public static void main(String[] args) {
+        System.out.println("测试主分支");
+    }
+}
