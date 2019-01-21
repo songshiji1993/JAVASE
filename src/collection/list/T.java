7f35b2a535829c0539fb91e9cf243e391ee0226e
song <songshiji@bupt.edu.cn>
2019-01-21 22:51:27 +0800

    集合

--- src/collection/list/T.java
+++ src/collection/list/T.java
@@ -2,6 +2,20 @@ package collection.list;
 
 public class T {
 
+/**
+ * Java集合分两个根接口
+ *
+ * 根接口 Collection                                                            Map
+ * 子接口         List(序列),Queue(队列)和Set(集)
+ * 实现类         ArrayList（数组序列），LinkedList(链表) LinkedList(链表)     HashSet
+ *
+ * List和Queue存储的元素排列有序的而且可以重复的。
+ *
+ *
+ *
+ */
+
+
 
 
 }
